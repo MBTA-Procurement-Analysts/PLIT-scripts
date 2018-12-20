678b235c99e9bb3d7ab77d2545f929ceeb69fec2
Rubix <nguo@mbta.com>
2018-12-20 13:55:24 -0500

    PO insertion works without deletion now

--- poDataInsertion.py
+++ poDataInsertion.py
@@ -47,27 +47,30 @@ for location in writelocation:
     dbname = 'rubix-' + serverlocation + '-' + location 
     print('Using database ' + dbname)
     db = client[dbname]
-
+    uniquePOIDs = dict((pono, False) for pono in insertionItems['PO_No'].unique().tolist())
     for row in tqdm(insertionItems.itertuples()):
-        db.PO_DATA.remove({'PO_No': row.PO_No})
-        if pd.isna(row.PO_AprvDate):
-            aprvDate = ""
-        else:
-            aprvDate = row.PO_AprvDate
-        db.PO_DATA.update({'PO_No': row.PO_No}, {
-            '$set': {
-                'PO_No': row.PO_No,
-                'Business_Unit': row.Business_Unit,
-                'Buyer': row.Buyer,
-                "Status": row.Status,
-                'PO_Date': row.PO_Date,
-                "Origin": row.Origin,
-                "Approved_By": row.Approved_By,
-                "Vendor": row.Vendor_Name,
-                "PO_Approval_Date": aprvDate
-            }
-        }, upsert=True)
+        if not uniquePOIDs[row.PO_No]:
+            if pd.isna(row.PO_AprvDate):
+                aprvDate = ""
+            else:
+                aprvDate = row.PO_AprvDate
+            db.PO_DATA.update({'PO_No': row.PO_No}, {
+                '$set': {
+                    'PO_No': row.PO_No,
+                    'Business_Unit': row.Business_Unit,
+                    'Buyer': row.Buyer,
+                    "Status": row.Status,
+                    'PO_Date': row.PO_Date,
+                    "Origin": row.Origin,
+                    "Approved_By": row.Approved_By,
+                    "Vendor": row.Vendor_Name,
+                    "PO_Approval_Date": aprvDate,
+                    "lines": []
+                }
+            }, upsert=True)
+            uniquePOIDs[row.PO_No] = True
     
+    for row in tqdm(insertionItems.itertuples()):
         db.PO_DATA.update({"PO_No": row.PO_No}, {
             '$push': {
                 "lines": {
@@ -86,7 +89,7 @@ for location in writelocation:
                     },
                     "Quantity": row.PO_Qty
                 }
-            }}, upsert=True)
+            }})
         # db.REQ_DATA.find({"lines": {$elemMatch: {"Item": "02545903"}}})
     
     db.LAST_UPDATED.update({'dbname': "PO_DATA"}, {'$set': {'last_updated_time': time.time()}})
