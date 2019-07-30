587b048505617d12e5ea427b565e0589ec7c4872
Rubix <nguo@mbta.com>
2019-07-30 12:35:19 -0400

    Added workflow (PO) scripts

---
+++ approvalWorkflow.py
@@ -0,0 +1,72 @@
+# approvalWorkflow.py
+# Created by Mickey G for Project Flextape use
+# To import Approveal Workflow data to mongodb, with flextape pipeline
+
+
+import pandas as pd
+import pymongo
+from pymongo import MongoClient
+from tqdm import tqdm
+import sys
+import time
+import os
+
+if not sys.argv[1] and not sys.argv[2]:
+    raise ValueError('Arguments Needed: Date String (mmddyyyy-hhmmss), Write Location: One of (dev, prod, both)')
+date = sys.argv[1]
+writelocation = ['dev', 'prod'] if sys.argv[2] == 'both' else [sys.argv[2]]
+
+serverlocation = os.environ['RUBIXLOCATION']
+
+if serverlocation == 'local':
+    filepathprefix = "/home/rubix/Desktop/Project-Ducttape/data/"
+elif serverlocation == 'ohio':
+    filepathprefix = "/home/ubuntu/Projects/flextape/"
+else:
+    raise EnvironmentError('Environment Variable "RUBIXLOCATION" seems not to be set.')
+
+filepath = filepathprefix + "approval_workflow/" + date + "/SLT_PO_WF-" + date + ".xlsx"
+
+print("--- Reading " + filepath + " ---")
+
+# Reads Excel File
+insertionItems = pd.read_excel(filepath, skiprows = 1)
+
+# Fills spaces in Column Names with underscores
+insertionItems.columns = [c.replace(' ', '_') for c in insertionItems.columns]
+insertionItems.columns = [c.replace('.', '') for c in insertionItems.columns]
+
+client = MongoClient()
+
+print("--- Pushing Data to Mongo ---")
+
+for location in writelocation:
+    dbname = "rubix-{}-{}".format(serverlocation, location)
+    print("Using database {}".format(dbname))
+    db = client[dbname]
+    # clear existing data
+    uniquePOArr = insertionItems['PO_No'].unique().tolist()
+    for po_num in uniquePOArr:
+        db.PO_DATA.update_one({"PO_No": po_num}, {'$set': {"worklist": []}})
+
+    for row in tqdm(insertionItems.itertuples()):
+        db.PO_DATA.update_one({"PO_No": row.PO_No}, {'$push': {"worklist": {
+                        "Appr_Inst": row.Appr_Inst,
+                        "Work_List": row.Work_List if not pd.isna(row.Work_List) else "",
+                        "Approval_Number": row.Approval_Number if not pd.isna(row.Approval_Number) else "",
+                        "Appr_Stat": row.Appr_Stat if not pd.isna(row.Appr_Stat) else "",
+                        "Denial_Date_Time": row.Denial_Date_Time,
+                        "User": row.User,
+                        "Unit": row.Unit,
+                        "PO_HDR_Status": row.PO_HDR_Status,
+                        "WF_APPR_Status": row.WF_APPR_Status,
+                        "Dispatch_DTTM": row.Dispatch_DTTM,
+                        "Threshold": row.Threshold,
+                        "Buyer": row.Buyer,
+                        "PO_Hdr_Created_Date": row.PO_Hdr_Created_Date,
+                        "SUM_MERCHANDISE_AMT": row.SUM_MERCHANDISE_AMT
+                    }}})
+
+    db.LAST_UPDATED.update_one({'dbname': "PO_DATA_WORKFLOW"}, {'$set': {'last_updated_time': time.time()}})
+
+print("--- PO Workflow Update Done ---")
---
+++ workListDataInsertion.py
@@ -0,0 +1,101 @@
+# poDataInsertion.py
+# Created by Christopher M for Project flextape use
+# To Import PO data to mongodb, within the flextape pipeline
+
+# On 06/28/2019, the script was modified to accomodate the addition
+#   of 'PO Approval Date' field.
+
+import pandas as pd
+import pymongo
+from pymongo import MongoClient
+from tqdm import tqdm
+import sys
+import time
+import os
+
+print("poDataInsertion.py taking over...")
+
+if not sys.argv[1] and not sys.argv[2]:
+    raise ValueError('Arguments Needed: Date String (mmddyyyy-hhmmss), Write Location: One of (dev, prod, both)')
+date = sys.argv[1]
+writelocation = ['dev', 'prod'] if sys.argv[2] == 'both' else [sys.argv[2]]
+
+serverlocation = os.environ['RUBIXLOCATION']
+
+if serverlocation == 'local':
+    filepathprefix = "/home/rubix/Desktop/Project-Ducttape/data/"
+elif serverlocation == 'ohio':
+    filepathprefix = "/home/ubuntu/Projects/flextape/"
+else:
+    raise EnvironmentError('Environment Variable "RUBIXLOCATION" seems not to be set.')
+
+filepath = filepathprefix + "pow/" + date + "/SLT_PO_WF_" + date + ".xlsx" 
+
+print("--- Reading " + filepath + " ---")
+
+insertionItems = pd.read_excel(filepath, skiprows=1)
+
+client = MongoClient()
+insertionItems.columns = [c.replace(' ', '_') for c in insertionItems.columns]
+insertionItems.columns = [c.replace('/', '_') for c in insertionItems.columns]
+insertionItems.columns = [c.replace('.', '') for c in insertionItems.columns]
+
+print("--- Pushing Data to mongo ---")
+
+print(insertionItems.columns)
+for location in writelocation:
+    dbname = 'rubix-' + serverlocation + '-' + location 
+    print('Using database ' + dbname)
+    db = client[dbname]
+    uniquePOIDs = dict((pono, False) for pono in insertionItems['PO_No'].unique().tolist())
+    for row in tqdm(insertionItems.itertuples()):
+        if not uniquePOIDs[row.PO_No]:
+                       
+            if pd.isna(row.Denial_Date_Time):
+                    DDT = ""
+            else:
+                DDT = row.Denial_Date_Time
+
+            if pd.isna(row.Dispatch_DTTM):
+                Dispatch_Date = ""
+            else: 
+                Dispatch_Date = row.Dispatch_DTTM
+
+            if pd.isna(row.PO_Hdr_Created_Date):
+                Created_Date = ""
+            else: 
+                Created_Date = row.PO_Hdr_Created_Date
+
+
+
+            db.PO_DATA.update({'PO_No': row.PO_No}, {
+                '$set': {
+                    'PO_No': row.PO_No,
+                    "worklist": []
+                }
+            }, upsert=True)
+            uniquePOIDs[row.PO_No] = True
+    
+    for row in tqdm(insertionItems.itertuples()):
+        db.PO_DATA.update({"PO_No": row.PO_No}, {
+            '$push': {
+                "worklist": {
+                    "Appr_Inst": row.Appr_Inst,
+                    "Work_List": row.Work_List,
+                    "Approval_Number": row.Approval_Number,
+                    "Appr_Stat": row.Appr_Stat,
+                    "Denial_Date_Time": DDT,
+                    "Lv_Appr": row.User,
+                    "Unit": row.Unit,
+                    "PO_HDR_Status": row.PO_HDR_Status,
+                    "WF_APPR_Status": row.WF_APPR_Status,
+                    "Dispatch_DTTM": Dispatch_Date,
+                    "PO_Hdr_Created_Date": Created_Date,
+                    "SUM_MERCHANDISE_AMT": row.SUM_MERCHANDISE_AMT,
+                    },
+                }
+            })
+    
+    db.LAST_UPDATED.update({'dbname': "PO_DATA"}, {'$set': {'last_updated_time': time.time()}})
+    
+    print("--- PO Update Done ---")
