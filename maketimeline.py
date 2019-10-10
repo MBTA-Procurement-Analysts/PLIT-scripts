42a28b87de0195ffc81068e741d8cdbaf70a1758
Nianmin Guo <nguo@MBTA.com>
2019-10-10 16:33:48 -0400

    timeline all components in (?)

--- maketimeline.py
+++ maketimeline.py
@@ -5,14 +5,15 @@
 #print("This is WIP. Do not run just yet")
 #exit()
 
-import pymongo
-from pymongo import MongoClient
+import os
+import re
 import sys
 from datetime import datetime
-import os
-from tqdm import tqdm
-import re
 from collections import Counter
+from tqdm import tqdm
+import pymongo
+from pymongo import MongoClient
+
 import pprint
 
 pp = pprint.PrettyPrinter()
@@ -26,7 +27,7 @@ PO_STATUS_DICT = {"A": "Approved",
              "PX": "Pending Cancel",
              "X": "Cancelled"}
 
-PO_WL_STATUS = {
+WL_STATUS = {
     "I": "Initiated",
     "A": "Approved",
     "D": "Denied"
@@ -35,10 +36,13 @@ PO_WL_STATUS = {
 # Regex pattern used to determine whether a PO Timeline is internal (on ____ field)
 internal_pattern = re.compile(r".*Procurement.*", re.I)
 
-class BundledPOError(Exception):
+class POBundledError(Exception):
     def __init__(self, msg):
-        self.msg = msg
+        self.msg = "[Bundled PO] {}".format(msg)
 
+class PONoLineError(Exception):
+    def __init__(self, msg):
+        self.msg = "[PO No Lines] {}".format(msg)
 
 def is_valid_po(po):
     # status is not canceled
@@ -52,20 +56,20 @@ def is_valid_po(po):
 
 
 def checkBundledPO(po):
+    if po.get("lines", None) is None:
+        raise PONoLineError("PO has no Line information. It might be a DBE shell with no corresponding PO.")
     reqs = set([line["Requisition_Data"]["Req_ID"] for line in po["lines"] if line.get("Requisition_Data", None)])
     if len(reqs) > 1:
-        raise BundledPOError("[BundledPO] {} REQs found with {}.".format(len(reqs), po["PO_No"]))
+        raise POBundledError("{} REQs found with {}.".format(len(reqs), po["PO_No"]))
 
 def getPOs(dbclient, req_lines_po_set):
     pos = []
     for po_num in req_lines_po:
-        print(po_num)
         for db_po in db.PO_DATA.find({"$and": [{"PO_No": po_num},
                                                {"Business_Unit": req["Business_Unit"]}]}):
             checkBundledPO(db_po)
             if is_valid_po(db_po):
                 pos.append(db_po)
-    
     return pos
 
 
@@ -132,26 +136,36 @@ def getPOEvents(po_arr):
 
 def getReqWorklistEvents(req_worklists):
     results = []
+    req_worklists = sorted(req_worklists, key=lambda k: (k["Appr_Inst"], k["Approval_Number"], k["Date_Time"]))
+    seen_timestamps = set()
+    # if len(set([ele["Appr_Inst"] for ele in req_worklists])) > 1:
+    #     pp.pprint(req_worklists)
     for req_wl in req_worklists:
+        eventtype = WL_STATUS.get(req_wl["Appr_Stat"], req_wl["Appr_Stat"])
+        auto_event = req_wl["Date_Time"] in seen_timestamps
+        if req_wl["Appr_Stat"] == "I":
+            text = "Req Initiated."
+        else:
+            text = "{}, {}".format(req_wl["Work_List"], req_wl["Approval_Number"])
         results.append({"ID": req_wl["Appr_Inst"],
-                        "Start_DTTM": req_wl["Denial_Date_Time"],
+                        "Start_DTTM": req_wl["Date_Time"],
                         "EventType": "",
-                        "Text": "",
-                        "Person": "",
+                        "Text": text,
+                        "Person": req_wl["User"],
                         "Internal": "",
                         "Neutral": "",
                         "Auto": False,
-                        "Lifecycle": ""})
+                        "Lifecycle": "REQ"})
     return results
 
 def getPOWorklistEvents(po_worklists):
     results = []
-    po_worklists = sorted(po_worklists, key=lambda k: (k["Event_Date_Time"], k["Approval_Number"]))
+    po_worklists = sorted(po_worklists, key=lambda k: (k["Appr_Inst"], k["Approval_Number"], k["Event_Date_Time"]))
     seen_timestamps = set()
-    #if len(set([ele["Event_Date_Time"] for ele in po_worklists])) != len(po_worklists):
-    #    pp.pprint(po_worklists)
+    # if len(set([ele["Event_Date_Time"] for ele in po_worklists])) != len(po_worklists):
+    #     pass
     for po_wl in po_worklists:
-        eventtype = PO_WL_STATUS.get(po_wl["Appr_Stat"], po_wl["Appr_Stat"])
+        eventtype = WL_STATUS.get(po_wl["Appr_Stat"], po_wl["Appr_Stat"])
         internal = internal_pattern.match(po_wl["Work_List"])
         auto_event = po_wl["Event_Date_Time"] in seen_timestamps
         if po_wl["Appr_Stat"] == "I":
@@ -174,9 +188,9 @@ def getPOWorklistEvents(po_worklists):
 
 def sort_events(tlevents):
     tlevents = sorted(tlevents, key= lambda ele: ele["Start_DTTM"])
-    for i in range(len(tlevents) - 1):
-        tlevents[i]["End_DTTM"] = tlevents[i+1]["Start_DTTM"]
-    tlevents[-1]["End_DTTM"] = datetime.now()
+    # for i in range(len(tlevents) - 1):
+    #     tlevents[i]["End_DTTM"] = tlevents[i+1]["Start_DTTM"]
+    # tlevents[-1]["End_DTTM"] = datetime.now()
 
 def write_complicated(db, reqNo, poNo, msg):
     db.TIMELINE.insert_one(
@@ -186,6 +200,8 @@ def write_complicated(db, reqNo, poNo, msg):
          "Business_Unit": "",
          "Footnote": "",
          "events": []})
+
+# ---------- MAIN FUNCTION BELOW ----------
 
 if not sys.argv[1]:
     raise ValueError('Arguments Needed: Write Database: One of (dev, local).')
@@ -205,14 +221,13 @@ for loc in writelocation:
     # For each REQ in the REQ DB
     for req in tqdm(db.REQ_DATA.find()):
         footnote = ""
-        if False and len(req.get("worklist", [])) == 0:
-            write_complicated(db,
-                              req["REQ_No"],
-                              "",
-                              "[NoWorklist] Req {} does not have a worklist.".format(req["REQ_No"]))
+        if len(req.get("worklist", [])) == 0:
+            # write_complicated(db,
+            #                   req["REQ_No"],
+            #                   "",
+            #                   "[REQNoWorklist] Req {} does not have a worklist.".format(req["REQ_No"]))
             continue
         # Find POs using REQ Lines, skip if multiple
-        print(req["REQ_No"])
         req_lines_po = set([line["PO"]["PO_Number"]
                             for line in req["lines"] if line.get("PO", None)])
         if len(req_lines_po) > 1:
@@ -225,7 +240,13 @@ for loc in writelocation:
         # Get POs, skip if none or none valid found
         try:
             pos = getPOs(db, req_lines_po)
-        except BundledPOError as e:
+        except POBundledError as e:
+            write_complicated(db,
+                              req["REQ_No"],
+                              "",
+                              e.msg)
+            continue
+        except PONoLineError as e:
             write_complicated(db,
                               req["REQ_No"],
                               "",
@@ -236,7 +257,7 @@ for loc in writelocation:
             write_complicated(db,
                               req["REQ_No"],
                               "",
-                              "No POs associated with REQ {} are valid. Did not insert in DB.".format(req["REQ_No"]))
+                              "[NoValidPO] No POs associated with REQ {} are valid. Did not insert in DB.".format(req["REQ_No"]))
             continue
 
         po_worklists = getPOWorklists(pos)
@@ -245,10 +266,10 @@ for loc in writelocation:
         req_evnets = getReqEvents(req) 
         po_events = getPOEvents(pos)
         
-        #req_worklist_events = getReqWorklistEvents(req_worklists)
+        req_worklist_events = getReqWorklistEvents(req_worklists)
         po_worklist_events = getPOWorklistEvents(po_worklists)
         if len(pos) > 0:
-            timeline_events = [*req_evnets, *po_events, *po_worklist_events]
+            timeline_events = [*req_evnets, *po_events, *req_worklist_events, *po_worklist_events]
             sort_events(timeline_events)
             db.TIMELINE.insert_one(
                 {"REQ_No": req["REQ_No"],
